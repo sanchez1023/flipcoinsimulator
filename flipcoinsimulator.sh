edae720850b0174e29c89565480d1da541d118fd
sanchez1023 <shubham1023@gmail.com>
2020-02-22 12:43:25 +0530

    Added coin flip to check result is heads or tails

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,3 +1,17 @@
 #/bin/bash -x
 
 echo "welcome to flip coin simulator"
+
+HEADS=1
+randomCheck=$((RANDOM % 2))
+       
+                if [ $HEADS -eq $randomCheck ]
+                then
+
+                        
+                        echo  coin flipped result is heads
+                else
+                        echo coin flipped result is tails
+                        
+
+                 fi
